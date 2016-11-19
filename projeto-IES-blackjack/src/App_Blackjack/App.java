9420461db1c6416dbc08836218670af265f974e8
marlonfurtado <marlonfurtado.rs@gmail.com>
2016-11-19 15:14:34 -0200

    #1 Decidir quem ganhou

--- projeto-IES-blackjack/src/App_Blackjack/App.java
+++ projeto-IES-blackjack/src/App_Blackjack/App.java
@@ -34,15 +34,19 @@ public class App {
 			aposta = in.nextInt();
 		}
 
-		// Passa para o jogador o nome e sua aposta
-		// AINDA TEM QUE FAZER O VALOR DA APOSTA DIMINUIR
-		// DO VALOR 'DEFAULT', QUE COMEÇA COM 100$ NA CLASSE 'Jogador'
-
+		// Passa para o jogador o 'nome' e sua 'aposta'
+		// O VALOR 'DEFAULT' DA APOSTA COMEÇA COM 100$ NA CLASSE 'Jogador'
+		// E DECREMENTA DO VALOR PASSADO POR PARAMETRO
 
 		JogadorController jogador = new JogadorController(nome, aposta);
 		Carta carta = new Carta();
 
+		
+		System.out.println("Seu dinheiro: "+jogador.getAposta()+"$");
+		System.out.println("Valor da sua aposta: "+aposta+"$\n");
 
+		
+		
 		// MENU PROVISORIO, DEPOIS DA PARA CRIAR UM MÉTODO
 		// SO PARA O MENU
 		System.out.println("1 - Solicitar cartas");
@@ -52,15 +56,17 @@ public class App {
 
 
 		// CONTADOR PARA DECIDIR QUAL RODADA ESTA O JOGADOR
-		int i = 1;
+		// TALVEZ NÃO SEJA MAIS NECESSARIO,
+		// SO VAI TER UMA RODADA ?!
+		int countRodada = 1;
 
 		if(opcao == 1){
 
 			System.out.println("\n--------------------");
-			System.out.println("RODADA "+i+"\n");
+			System.out.println("RODADA "+countRodada+"\n");
 
 
-			// VETOR PARA GUARDAR AS CARTAS DO JOGADO E DEALER
+			// VETOR PARA GUARDAR AS CARTAS DO JOGADOR E DEALER
 			// ESTAO NA CLASSE 'JogadorController'
 			// AINDA FALTA FAZER AS SOMAS E DECIDIR SE O JOGADOR GANHOU OU PERDEU
 
@@ -72,8 +78,8 @@ public class App {
 
 			// NA CLASSE 'Carta' AS CARTAS SAO GERADAS DE FORMA 'RANDOM'
 			// CADA VEZ Q CHAMAR O METODO 'gerarCarta', VAI VIR UMA CARTA DIFERENTE ALEATORIA
-			// APOS CHAMAR O METODO 'gerarCarta', A CARTA E ADICIONADA NO VETOR
-			// POR ISSO TEM Q CHAMAR E ADICIONAR NO VETOR
+			// APOS CHAMAR O METODO 'gerarCarta', A CARTA É ADICIONADA NO VETOR
+			// POR ISSO TEM Q CHAMAR E LOGO JA ADICIONAR NO VETOR
 			// DEPOIS CHAMA O 'setCartasJogador' PARA MANDAR ESSE VETOR PRA CLASSE 'JogadorController'
 
 			cartasJogador.add(carta.gerarCarta().getNumero() + carta.gerarCarta().getNaipe());
@@ -99,7 +105,7 @@ public class App {
 
 
 
-			// MENU PARA ENTRA NO 'SWITCH'
+			// MENU PARA ENTRAR NO 'SWITCH'
 
 			System.out.println("1 - Mais uma carta");
 			System.out.println("2 - Encerrar rodada");
@@ -134,51 +140,122 @@ public class App {
 					
 					System.out.println("1 - Mais uma carta");
 					System.out.println("2 - Encerrar rodada");
+					System.out.println("0 - Sair");
 					System.out.println("Digite sua opção: ");
 
 
 					opcao = in.nextInt();
-
-					i++;
 
 					break;
 				case 2:
 					// ENCERRAR RODADA
 					// FAZER AS CONTAS
 					// DECIDIR QUEM GANHOU
-					int somarCartas = 0;
+					
+					int somarCartasJogador = 0;
+					int somarCartasSistema = 0;
 
+					
+					// ADICIONAR CARTAS PARA O SISTEMA
+					for(int i = 0; i < cartasJogador.size(); i++){
+						cartasSistema.add(carta.gerarCarta().getNumero() + carta.gerarCarta().getNaipe());
+					}
+					
+					
+					
+					// 'FOR' PARA SOMAR AS CARTAS DO JOGADOR
 					for (String c : cartasJogador) {
 						
 						c = Character.toString(c.charAt(0));
 						int auxC = Integer.parseInt(c);
 						
-						somarCartas += auxC;
+						somarCartasJogador += auxC;
 					}
 					
-					if (somarCartas == 21){
-						System.out.println("\nSoma das cartas: "+somarCartas);
+					// PRINTAR CARTAS DO SISTEMA
+					System.out.print("Cartas do sistema:  ");
+					for (String c : cartasSistema) {
+						System.out.print(c);
+					}
+					System.out.println();
+
+					// 'FOR' PARA SOMAR AS CARTAS DO SISTEMA
+					for (String c : cartasSistema) {
+						
+						c = Character.toString(c.charAt(0));
+						int auxC = Integer.parseInt(c);
+						
+						somarCartasSistema += auxC;
+					}
+					
+					
+					
+					// EMPATE
+					if (somarCartasJogador == somarCartasSistema || (somarCartasJogador > 21 && somarCartasSistema > 21)){
+						System.out.println("\nSoma das cartas jogador: "+somarCartasJogador);
+						System.out.println("Soma das cartas sistema: "+somarCartasSistema);
+						System.out.println();
+
+						System.out.println("EMPATE!");
+						System.out.println("JOGADOR RECEBE DE VOLTA DINHEIRO DA APOSTA");
+						System.out.println("AINDA FALTA ARRUMAR...");
+					
+						opcao = 0;
+						break;
+					}
+					
+					// JOGADOR GANHA COM 21
+					else if (somarCartasJogador == 21){
+						System.out.println("\nSoma das cartas jogador: "+somarCartasJogador);
+						System.out.println("Soma das cartas sistema: "+somarCartasSistema);
+						System.out.println();
+
+						System.out.println("'GANHOU COM CARTA IGUAL A 21'");
 						System.out.println("Parabens! Você ganhou");
 						System.out.println("'FALTA ARRUMAR A MASCADA QUE ELE VAI RECEBER'");
 						opcao = 0;
 						break;
 					}
-					else if (somarCartas > 21){
-						System.out.println("\nSoma das cartas: "+somarCartas);
-						System.out.println("Você provavelmente perdeu, mas ainda falta"
-								+ "arrumar pra ver as cartas se o dealer ganhou ou nao");
-						opcao = 0;
-						break;
-					}
-					else if(somarCartas < 21){
-						System.out.println("\nSoma das cartas: "+somarCartas);
-						System.out.println("Ainda falta arrumar pra ver as cartas do Dealer "
-								+ "e ver quem ganhou");
+					
+					// JOGADOR GANHA COM CARTA MAIOR Q O SISTEMA
+					else if(somarCartasJogador < 21 && (somarCartasJogador > somarCartasSistema)){
+						System.out.println("\nSoma das cartas jogador: "+somarCartasJogador);
+						System.out.println("Soma das cartas sistema: "+somarCartasSistema);
+						System.out.println();
+
+						System.out.println("'GANHOU COM CARTA MAIOR Q O SISTEMA'");
+						System.out.println("Parabens! VOce ganhou");
 						System.out.println("E ver o dinheiro das apostas");
 
 						opcao = 0;
 						break;
 					}
+					
+					// JOGADOR PERDE PQ TEM CARTA MAIOR QUE 21
+					else if (somarCartasJogador > 21 && somarCartasSistema <= 21){
+						System.out.println("\nSoma das cartas jogador: "+somarCartasJogador);
+						System.out.println("Soma das cartas sistema: "+somarCartasSistema);
+						System.out.println();
+
+						System.out.println("'PERDEU COM CARTA MAIOR Q 21'");
+						System.out.println("PERDEU DINHEIRO, O VALOR DO SEU DINHEIRO É: "+jogador.getAposta());
+						opcao = 0;
+						break;
+					}
+					
+					// JOGADOR PERDE COM CARTA MENOR Q O SISTEMA
+					else if((somarCartasJogador < 21 && somarCartasSistema <= 21) && (somarCartasJogador < somarCartasSistema)){
+						System.out.println("\nSoma das cartas jogador: "+somarCartasJogador);
+						System.out.println("Soma das cartas sistema: "+somarCartasSistema);
+						System.out.println();
+
+						System.out.println("'PERDEU COM CARTA MENOR Q O SISTEMA'");
+						System.out.println("PERDEU DINHEIRO, O VALOR DO SEU DINHEIRO É: "+jogador.getAposta());
+
+						opcao = 0;
+						break;
+					}
+					
 					
 				case 0:
 					System.out.println("SAIND DO JOGO NA GAMBIARRA.. TRATAR ISSO");
