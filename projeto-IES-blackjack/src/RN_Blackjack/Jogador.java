465e241dc806dd0103fa32ef37fd2e22e9395e1c
matheusmorcinek <matheusmorcinek@gmail.com>
2016-11-19 23:26:05 -0200

    Projeto base. - atualizações.

--- projeto-IES-blackjack/src/RN_Blackjack/Jogador.java
+++ projeto-IES-blackjack/src/RN_Blackjack/Jogador.java
@@ -1,28 +1,2 @@
 package RN_Blackjack;
 
-public class Jogador {
-
-	private String nome;
-	private int aposta = 100;
-
-	public Jogador(String nome, int aposta){
-		this.nome = nome;
-		this.aposta -= aposta;
-	}
-	
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public int getAposta() {
-		return aposta;
-	}
-
-	public void setAposta(int aposta) {
-		this.aposta = aposta;
-	}
-	
-}
