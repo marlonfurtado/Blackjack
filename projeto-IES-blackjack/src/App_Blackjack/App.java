0f087bc4254701033a0de802aec31f6bf46e4fd4
marlonfurtado <marlonfurtado.rs@gmail.com>
2016-11-20 14:56:49 -0200

    #

--- projeto-IES-blackjack/src/App_Blackjack/App.java
+++ projeto-IES-blackjack/src/App_Blackjack/App.java
@@ -1,48 +1,67 @@
 package App_Blackjack;
 
 
+import java.io.IOException;
 import java.util.Scanner;
 
+import DB_Blackjack.ConexaoDB;
 import RN_Blackjack.Banca;
 import RN_Blackjack.Jogador;
 
 
 public class App {
-    
-   	public static void main(String[] args) {
 
-            
-                BlackJack blackjack = new BlackJack();
-                
-                Scanner in = new Scanner(System.in);
+	public static void main(String[] args) {
 
-		System.out.println("\nBem vindo ao Jogo 21 (Blackjack)");
-		System.out.println("Entre com seu nome e faça sua aposta!");
 
-		System.out.print("Digite seu nome: ");
-		String nome = in.next();
+		BlackJack blackjack = new BlackJack();
+
+		Scanner in = new Scanner(System.in);
+
+		try{
+			new ConexaoDB();
+
+			System.out.println("\nBem vindo ao Jogo 21 (Blackjack)");
+			System.out.println("Entre com seu nome e faÃ§a sua aposta!");
+			ConexaoDB.salvarJogo.write("\nBem vindo ao Jogo 21 (Blackjack)");
+			ConexaoDB.salvarJogo.newLine();
+			ConexaoDB.salvarJogo.write("Entre com seu nome e faÃ§a sua aposta!");
+
+
+			System.out.print("Digite seu nome: ");
+			String nome = in.next();
 			//jogador
-                Jogador jogador = new Jogador(nome);
-                Banca banca = new Banca();
-                
-                System.out.println("\nOlá "+jogador.getNome()+" você possui R$"+jogador.getDinheiro() +" para apostas.\n");
+			Jogador jogador = new Jogador(nome);
+			Banca banca = new Banca();
 
-                    int opcao;
-                    do{
-                        
-                        System.out.println("1 - Iniciar rodada");
-                        System.out.println("0 - Sair");
-                        System.out.println("Digite sua opçao: ");
-                        opcao = in.nextInt();
-                        
-                        if(opcao == 1){
-                            System.out.println("");
-                            double aposta = 0;
-                            System.out.println("\nDigite o valor da sua aposta: ");
-                            aposta = in.nextDouble();
-                            blackjack.criaRodada(aposta,jogador,banca);        
-                        }
-                                               
-                    }while(opcao != 0);                
-        }
+			System.out.println("\nOlÃ¡ "+jogador.getNome()+" vocÃª possui R$"+jogador.getDinheiro() +" para apostas.\n");
+			ConexaoDB.salvarJogo.newLine();
+			ConexaoDB.salvarJogo.write("\nOlÃ¡ "+jogador.getNome()+" vocÃª possui R$"+jogador.getDinheiro() +" para apostas.\n");
+			ConexaoDB.salvarJogo.newLine();
+
+			int opcao;
+			do{
+
+				System.out.println("1 - Iniciar rodada");
+				System.out.println("0 - Sair");
+				System.out.println("Digite sua opï¿½ao: ");
+				opcao = in.nextInt();
+
+				if(opcao == 1){
+					System.out.println("");
+					double aposta = 0;
+					System.out.println("\nDigite o valor da sua aposta: ");
+					aposta = in.nextDouble();
+					blackjack.criaRodada(aposta,jogador,banca);        
+				}
+
+			}while(opcao != 0);  
+		
+			ConexaoDB.salvarJogo();
+			
+		}catch(IOException e){
+			e.getMessage();
+		}
+	
+	}
 }
